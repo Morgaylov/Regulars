7aec5c10af3a09716ed913832bb9f7eadccd9bff
Morgaylov <morgaylov.ids@gmail.com>
2014-01-29 12:22:25 +0200

    Variables were renamed according to JS Code Conventions

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /**
  * Created by Dmitry on 29.01.14.
  */
-var regexpparser = require('./regexpparser');
+var regexpParser = require('./regexpparser');
 
-var res = regexpparser.getMatches("In theoretical computer sex science and formal language theory, a regular expression (abbreviated regex or regexp) " +
+var res = regexpParser.getMatches("In theoretical computer sex science and formal language theory, a regular expression (abbreviated regex or regexp) " +
                                   "is a sequence of characters that forms a search pattern, mainly for use in pattern matching with strings," +
                                   "or string matching-like operations. The concept arose in the 1950s, when the American mathematician Stephen Kleene " +
                                   "formalized the description of a regular language, and came into common use with the Unix text processing utilities. "+
