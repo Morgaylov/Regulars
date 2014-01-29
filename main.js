14cf8332296cde600537cecf9fcb0bd95c9a3d8c
Morgaylov <morgaylov.ids@gmail.com>
2014-01-29 12:01:33 +0200

    Module parser.js was renamed to regexpparser.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /**
  * Created by Dmitry on 29.01.14.
  */
-var parser = require('./parser');
+var regexpparser = require('./regexpparser');
 
-var res = parser.getMatches("In theoretical computer sex science and formal language theory, a regular expression (abbreviated regex or regexp) " +
-                            "is a sequence of characters that forms a search pattern, mainly for use in pattern matching with strings," +
-                            "or string matching-like operations. The concept arose in the 1950s, when the American mathematician Stephen Kleene " +
-                            "formalized the description of a regular language, and came into common use with the Unix text processing utilities. "+
-                            "An editor, and grep (global regular expression print), a filter. American president.", /(sex)|(president)/gi);
+var res = regexpparser.getMatches("In theoretical computer sex science and formal language theory, a regular expression (abbreviated regex or regexp) " +
+                                  "is a sequence of characters that forms a search pattern, mainly for use in pattern matching with strings," +
+                                  "or string matching-like operations. The concept arose in the 1950s, when the American mathematician Stephen Kleene " +
+                                  "formalized the description of a regular language, and came into common use with the Unix text processing utilities. "+
+                                  "An editor, and grep (global regular expression print), a filter. American president.", /(sex)|(president)/gi);
 console.log(res);
